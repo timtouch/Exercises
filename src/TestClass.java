77991e079a42e0986290b0737e058f22829de8f5
Timothy Touch <chaosbounder@gmail.com>
2017-01-01 01:59:09 -0800

    Added Craps Excercise

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,9 +1,21 @@
+import java.util.Scanner;
+
 /**
  * Created by timtouch on 12/18/16.
  */
 public class TestClass {
     public static void main(String[] args) {
-        KnightsTour tour = new KnightsTour();
-        tour.solveKnightsTour();
+        Dice dices = new Dice(2);
+        CrapsPlayer player = new CrapsPlayer(10000);
+        Craps craps = new Craps(player, dices);
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Input number:");
+        int num = scan.nextInt();
+        System.out.println(num);
+
+        //craps.startGame();
+
     }
 }
